03b353e366f37c875dc6f55168d7fb8954a8694c
Rustam1616 <data.rustam.nazarov@gmail.com>
2023-11-19 01:42:35 +0400

    8

--- stock_market.py
+++ stock_market.py
@@ -37,7 +37,7 @@ for nums in mark.find_all('td', attrs = {"td-right"}):
         big_list.append(int(nums))
     x=x+1
     y=y+1
-if st.button('start'):
+if st.siddebar.button('start'):
     if proc == 'use':
         for link in mark.find_all('div', attrs = {"logo-container"}):
             photolink = 'https://companiesmarketcap.com'+link.find('img').get('src')
